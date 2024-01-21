0e0a2fdd157cf0b0ec9e030f0558ecb1d3ecced1
Ivan Vuksic <ivuksic@gmail.com>
2024-01-21 14:18:17 +0100

    done

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const questions = [
     question: "Which is the largest desert in the world?",
     answers: [
       { text: "Kalahari", correct: false },
-      { text: "Gobi", correct: true },
+      { text: "Gobi", correct: false },
       { text: "Sahara", correct: false },
       { text: "Antarctica", correct: true },
     ],
@@ -80,6 +80,7 @@ function selectAnswer(e) {
   const isCorrect = selectedBtn.dataset.correct === "true";
   if (isCorrect) {
     selectedBtn.classList.add("correct");
+    score++;
   } else {
     selectedBtn.classList.add("incorrect");
   }
@@ -91,5 +92,29 @@ function selectAnswer(e) {
   });
   nextButton.style.display = "block";
 }
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
 
 startQuiz();
