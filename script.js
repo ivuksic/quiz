fafe8ac7120c4f1afd8f6efc8b3f41b3808db045
Ivan Vuksic <ivuksic@gmail.com>
2024-01-21 14:00:31 +0100

    anwser buttons

--- script.js
+++ script.js
@@ -62,6 +62,10 @@ function showQuestion() {
     button.innerHTML = answer.text;
     button.classList.add("btn");
     answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
   });
 }
 function resetState() {
@@ -70,4 +74,22 @@ function resetState() {
     answerButtons.removeChild(answerButtons.firstChild);
   }
 }
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
 startQuiz();
